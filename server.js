c2ef352b0866680be384ae8579ac2d9055c212e2
Frews2 <parkersjames87@gmail.com>
2021-10-04 23:21:45 -0500

    Eliminar dependencias a rutas placeholder

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
  Author(s): Ricardo Moguel Sanchez
 */
 import express from "express";
-import accountRouter from './routes/accounts.js';
+/*import accountRouter from './routes/accounts.js';
 import newsRouter from './routes/news.js';
-import publicationRouter from './routes/publications.js';
+import publicationRouter from './routes/publications.js';*/
 import imageRouter from "./routes/images.js";
+import cors from 'cors';
 import fileupload from "express-fileupload";
 const app = express();
 const PORT = 4000;
@@ -26,10 +27,10 @@ app.use(cors());
 app.use(fileupload());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
-
+/*
 app.use("/accounts", cors(corsOptionsDelegate), accountRouter);
 app.use("/news", cors(corsOptionsDelegate), newsRouter);
-app.use("/publication", cors(corsOptionsDelegate), publicationRouter);
+app.use("/publication", cors(corsOptionsDelegate), publicationRouter);*/
 app.use("/images", cors(corsOptionsDelegate), imageRouter);
 
 app.all("*", cors(corsOptionsDelegate), (req, res) => res.status(404).send({
