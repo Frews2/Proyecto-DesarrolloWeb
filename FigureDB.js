08178b82d636fcbd631e095155a3c392572705e8
Frews2 <parkersjames87@gmail.com>
2021-10-04 23:12:39 -0500

    Update FigureDB.js

--- FigureDB.js
+++ FigureDB.js
@@ -10,6 +10,10 @@ db.Contrasenia.insert([
     {
         "Id": "b390be36-2004-4c86-8c5c-26d301693635",
         "Password": "raa"
+    },
+    {
+        "Id": "b390be36-2004-4c86-8c5c-26d301693699",
+        "Password": "word"
     }
 ])
 
@@ -17,22 +21,26 @@ db.Cuenta.insert([
     {
         "Id": "39c2d6c5-cdaa-48e8-a231-8a60f59391c5",
         "Email": "sam@gmail.com",
-        "TipoCuenta": "periodista"
+        "TipoCuenta": "periodista",
+        "PasswordId": "e04b7d73-3908-4b94-8df1-9b6dc71f38c5"
     },
     {
         "Id": "6f2850f9-b82f-451d-baf2-26fd93874418",
         "Email": "frews@gmail.com",
-        "TipoCuenta": "coleccionista"
+        "TipoCuenta": "coleccionista",
+        "PasswordId": "b390be36-2004-4c86-8c5c-26d301693635"
     },
     {
         "Id": "89e886e9-e46c-4e59-a1fb-e5bae1ead90f",
         "Email": "marcos@gmail.com",
-        "TipoCuenta": "coleccionista"
+        "TipoCuenta": "coleccionista",
+        "PasswordId": "6fb9c703-a712-434e-8619-cf06c80828b8"
     },
     {
         "Id": "2fda76ac-6382-41d4-8208-2e4324c495fc",
         "Email": "pklove@gmail.com",
-        "TipoCuenta": "administrador"
+        "TipoCuenta": "administrador",
+        "PasswordId": "b390be36-2004-4c86-8c5c-26d301693635"
     }
 ])
 
@@ -50,7 +58,7 @@ db.Periodista.insert([
         "Ocupacion": "Reportero",
         "FechaRegistro": "2021-09-29",
         "FechaNacimiento": "2000-09-11",
-        "Foto": " ",
+        "Foto": "rutafake",
         "Pais": "Mexico",
         "Sexo": "Masculino"
     }
@@ -62,7 +70,7 @@ db.Coleccionista.insert([
         "Apodo": "Frews",
         "FechaRegistro": "2021-10-01",
         "FechaNacimiento": "1998-05-30",
-        "Foto": "",
+        "Foto": "rutafake",
         "Pais": "Canada",
         "Sexo": "Masculino",
         "Estatus": "Activo"
@@ -72,7 +80,7 @@ db.Coleccionista.insert([
         "Apodo": "Marcos",
         "FechaRegistro": "2021-10-04",
         "FechaNacimiento": "1999-12-22",
-        "Foto": "",
+        "Foto": "rutafake",
         "Pais": "Panama",
         "Sexo": "Masculino",
         "Estatus": "Activo"
@@ -86,7 +94,7 @@ db.Figura.insert([
         "Altura": 40,
         "Material": "Acero Inoxidable",
         "Marca": "Guason Inc.",
-        "Foto": " "
+        "Foto": "rutafake"
     }
 ])
 
@@ -104,7 +112,7 @@ db.Noticia.insert([
         "IdPeriodista": "39c2d6c5-cdaa-48e8-a231-8a60f59391c5",
         "IdFigura": "b763f4b3-f67a-498e-917d-f74b4affb19e",
         "Texto": "Nueva figura de batman plateado",
-        "Foto": " ",
+        "Foto": "rutafake",
         "FechaPublicacion": "2021-10-03",
         "Etiquetas": ["DC","Batman","Metal"]
     }
@@ -136,7 +144,7 @@ db.Review.insert([
         "IdPublicacion": "5b8e53b1-94e1-4b97-a2fc-7b9078133e76",
         "Texto": "Review sobre el nuevo Batman 2021",
         "Calificacion": " 3.5 de 5 estrellas",
-        "Foto":" ",
+        "Foto":"rutafake",
         "Etiquetas": ["DC","Batman","Metal"]
     }
 ])
