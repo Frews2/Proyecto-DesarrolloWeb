493c03e37306f05be4b7681db3611d6af0201566
Frews2 <parkersjames87@gmail.com>
2021-10-19 02:41:32 -0500

    Modificar entidad cuentas a incluir atributos de usuario

--- FigureDB.js
+++ FigureDB.js
@@ -1,6 +1,6 @@
 var connection = connect("mongodb://figureAdmin:proyectoweb@localhost/admin");
 db = connection.getSiblingDB('FigureDB')
-db.Cuentas.insertMany([
+db.cuentas.insertMany([
     {
         "Id": "39c2d6c5-cdaa-48e8-a231-8a60f59391c5",
         "Email": "sam@gmail.com",
@@ -35,45 +35,13 @@ db.Cuentas.insertMany([
     }
 ])
 
-db.Administradores.insert([
+db.administradores.insert([
     {
         "IdCuenta": "2fda76ac-6382-41d4-8208-2e4324c495fc"
     }
 ])
 
-db.Periodistas.insert([
-    {
-        "IdCuenta": "39c2d6c5-cdaa-48e8-a231-8a60f59391c5",
-        "Nombre": "Samuel Juarez",
-        "Ocupacion": "Reportero",
-        "FechaRegistro": "2021-09-29",
-        "FechaNacimiento": "2000-09-11",
-        "Foto": "rutafake",
-        "Pais": "Mexico",
-        "Sexo": "Masculino"
-    }
-])
-
-db.Coleccionistas.insertMany([
-    {
-        "IdCuenta": "6f2850f9-b82f-451d-baf2-26fd93874418",
-        "FechaRegistro": "2021-10-01",
-        "FechaNacimiento": "1998-05-30",
-        "Foto": "rutafake",
-        "Pais": "Canada",
-        "Sexo": "Masculino"
-    },
-    {
-        "IdCuenta": "89e886e9-e46c-4e59-a1fb-e5bae1ead90f",
-        "FechaRegistro": "2021-10-04",
-        "FechaNacimiento": "1999-12-22",
-        "Foto": "rutafake",
-        "Pais": "Panama",
-        "Sexo": "Masculino"
-    }
-])
-
-db.Figuras.insert([
+db.figuras.insert([
     {
         "Id": "b763f4b3-f67a-498e-917d-f74b4affb19e",
         "Nombre": "Batman 2021 edicion especial",
@@ -84,7 +52,7 @@ db.Figuras.insert([
     }
 ])
 
-db.Colecciones.insert([
+db.colecciones.insert([
     {
         "Id": "b763f4b3-f67a-498e-917d-f74b4affb19e",
         "IdPropietario": "Batman 2021 edicion especial",
@@ -92,7 +60,7 @@ db.Colecciones.insert([
     }
 ])
 
-db.Noticias.insert([
+db.noticias.insert([
     {
         "Id": "a4d3c395-f2e8-41c3-8fc1-9e8362b088d4",
         "IdPeriodista": "39c2d6c5-cdaa-48e8-a231-8a60f59391c5",
@@ -104,7 +72,7 @@ db.Noticias.insert([
     }
 ])
 
-db.Publicaciones.insertMany([
+db.publicaciones.insertMany([
     {
         "Id": "296586c3-f036-49f8-b6c0-4f051de6c90a",
         "IdColeccionista": "6f2850f9-b82f-451d-baf2-26fd93874418",
@@ -117,7 +85,7 @@ db.Publicaciones.insertMany([
     }
 ])
 
-db.Comentarios.insert([
+db.comentarios.insert([
     {
         "IdPublicacion": "296586c3-f036-49f8-b6c0-4f051de6c90a",
         "Texto": "Buen review",
@@ -125,7 +93,7 @@ db.Comentarios.insert([
     }
 ])
 
-db.Reviews.insert([
+db.reviews.insert([
     {
         "IdPublicacion": "5b8e53b1-94e1-4b97-a2fc-7b9078133e76",
         "Texto": "Review sobre el nuevo Batman 2021",
@@ -135,7 +103,7 @@ db.Reviews.insert([
     }
 ])
 
-db.Reportes.insert([
+db.reportes.insert([
     {
         "Id": "7e15a339-c2b0-4210-b82c-a79f6db5f7ee",
         "IdPublicacion": "296586c3-f036-49f8-b6c0-4f051de6c90a",
